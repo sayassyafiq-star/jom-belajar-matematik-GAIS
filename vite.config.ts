be8a22e324118e4007112861709b8bdabc235931
Muhammad Syafiq <sayassyafiq@gmail.com>
2026-08-16 00:57:48 +0800

    ci: deploy Sprint 11 to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/jom-belajar-matematik-GAIS/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
